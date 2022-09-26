11c01f8ef5e33bb59eacc115ce1580746e7f4772
Skitten168 <kittens@mail.nmc.edu>
2022-09-25 22:30:02 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Backpackers_Authority.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
